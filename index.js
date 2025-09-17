f27b61f068c749e47ee97abe4cc2a26c7998ade0
MuqeemNasir <mac786m@gmail.com>
2025-09-17 09:11:31 +0530

    modified cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Hotel = require('./models/hotel.models')
 app.use(express.json())
 
 const corsOptions = {
-    origin: "https://hotel-frontend-theta-three.vercel.app",
+    origin: "https://hotel-frontend-9spy0wlwm-mohd-muqeem-nasirs-projects.vercel.app/",
     credentials: true,
     optionSuccessStatus: 200,
 } 
